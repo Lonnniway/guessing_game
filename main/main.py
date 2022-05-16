d02cfe6cef9ad9b97cdc2195a9d9904ff442beed
Lonnniway <gosha.siberia@gmail.com>
2022-05-16 21:06:45 +0700

    add results table

--- main/main.py
+++ main/main.py
@@ -1,4 +1,7 @@
 import random
+import time
+
+Rating = []
 
 def is_valid(a, key, Dip):
     if a == key:
@@ -14,22 +17,31 @@ def is_valid(a, key, Dip):
 
 print("Добро пожаловать в мою игру: 'Числовая угодайка' ")
 while True:
-    print("\n\ncase 1: Start game\ncase 2: Restart game\ncase 0: Exit")
+    print("\n\ncase 1: Start game\ncase 2: Restart game\ncase 3: Results table\ncase 0: Exit")
     case = input("case: ")
     print("\n")
 
     if case == "2":
         case = "1"
 
+    if case == '3':
+        for i in range(len(Rating)):
+            print(str(i+1)+')', Rating[i])
+
     if case == '1':
+        score = 0
         key = random.randint(1, 100)
         print("Компьютер загадал число, игра началась.")
 
         Diapason = [1, 100]
         x = int(input("Введите число: "))
+        score += 1
         while is_valid(x, key, Diapason) != True:
             x = int(input("Введите число: "))
-        print("Поздравляем, вы нашли загаданное число!")
+            score += 1
+        print("Поздравляем, вы нашли загаданное число!", 'ваши очки: ', score)
+        res = 'score: ' + str(score) + '  date: ' + time.ctime()
+        Rating.append(res)
 
     if case == '0':
         break
