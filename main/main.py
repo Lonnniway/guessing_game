dac255acc8da7929c77593887e6b512df9139e54
Lonnniway <gosha.siberia@gmail.com>
2022-05-16 20:46:52 +0700

    add diaposons

--- main/main.py
+++ main/main.py
@@ -1,16 +1,20 @@
 import random
 
-def is_valid(a, key):
+def is_valid(a, key, Dip):
     if a == key:
         return True
     if a < key:
-        print("Ваше число мало, возьмите больше.")
+        Dip[0] = a
+        print("Ваше число мало, возьмите больше", Dip)
     if a > key:
-        print("Ваше число слишком большое, возьмите меньше.")
+        Dip[1] = a
+        print("Ваше число слишком большое, возьмите меньше", Dip)
+
+
 
 print("Добро пожаловать в мою игру: 'Числовая угодайка' ")
 while True:
-    print("case 1: Start game\ncase 2: Restart game\ncase 0: Exit")
+    print("\n\ncase 1: Start game\ncase 2: Restart game\ncase 0: Exit")
     case = input("case: ")
     print("\n")
 
@@ -21,8 +25,9 @@ while True:
         key = random.randint(1, 100)
         print("Компьютер загадал число, игра началась.")
 
+        Diapason = [1, 100]
         x = int(input("Введите число: "))
-        while is_valid(x, key) != True:
+        while is_valid(x, key, Diapason) != True:
             x = int(input("Введите число: "))
         print("Поздравляем, вы нашли загаданное число!")
 
